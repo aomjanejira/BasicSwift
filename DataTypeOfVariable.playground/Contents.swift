27f70cb68c7f4f525e8a0bc305f9180f06560245
aom <aomiris1998@gmail.com>
2019-03-18 14:28:20 +0700

    Start DataType

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon"  //นี้คือการประกาศที่อาศัยชนิดของ Value บอก Datatype
+var mySurname: String = "AAAAAA" //นี้คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+
+
+
+
---
+++ VarialbleAndContent.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicait Variable
+//การประกาศตัวแปร
+
+var myName = "aom"
+myName = "Doramon"
+
+//Explicait Constant
+let myFriend = "Nopita"
+//myFriend = "Sunako" Error เพราะ  myFrind เป็นค่าคงที่ let
+
